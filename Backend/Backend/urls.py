d923487c0bf3a4681d7f334cdeb430ec5339e274
VladiShm <114508981+VladiShm@users.noreply.github.com>
2024-06-07 00:00:02 +0500

    добавлены шаблоны страниц регистрации, авторизации

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -11,7 +11,8 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to uвrlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import the include() fu
+    nction: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
